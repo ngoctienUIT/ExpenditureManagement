6b0eb7bc6347babbc59677370cdeb980637fb98c
Trần Ngọc Tiến <94039133+ngoctienUIT@users.noreply.github.com>
2022-09-19 08:36:51 +0700

    connect firebase
    Kết nối Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:expenditure_management/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
