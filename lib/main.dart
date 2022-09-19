1ade4461e64e908bbfbaf0f18feb88460351964d
Trần Ngọc Tiến <94039133+ngoctienUIT@users.noreply.github.com>
2022-09-19 09:44:44 +0700

    setup facebook login
    cài đặt để có thể đăng nhập bằng facebook

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Expenditure Management',
       theme: ThemeData(
         // This is the theme of your application.
         //
