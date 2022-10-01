138e39a814d2c2a1c74d7153afc48edddbb04f13
Trần Ngọc Tiến <94039133+ngoctienUIT@users.noreply.github.com>
2022-10-01 16:55:36 +0700

    update calendar and fix bug
    cập nhật lại giao diện xíu + fix 1 số lỗi

--- lib/page/main/calendar/calendar_page.dart
+++ lib/page/main/calendar/calendar_page.dart
@@ -3,6 +3,7 @@ import 'package:expenditure_management/constants/app_colors.dart';
 import 'package:expenditure_management/controls/spending_firebase.dart';
 import 'package:expenditure_management/models/spending.dart';
 import 'package:expenditure_management/page/main/calendar/widget/build_spending.dart';
+import 'package:expenditure_management/page/main/calendar/widget/custom_table_calendar.dart';
 import 'package:expenditure_management/page/main/calendar/widget/total_spending.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -37,14 +38,14 @@ class _CalendarPageState extends State<CalendarPage> {
                 var data = snapshot.requireData.data() as Map<String, dynamic>;
                 List<String> list = [];
 
-                if (data[DateFormat("MM_yyyy").format(_selectedDay)] != null) {
-                  list = (data[DateFormat("MM_yyyy").format(_selectedDay)]
-                          as List<dynamic>)
-                      .map((e) => e.toString())
-                      .toList();
-                }
-
                 return StatefulBuilder(builder: (context, setState) {
+                  if (data[DateFormat("MM_yyyy").format(_focusedDay)] != null) {
+                    list = (data[DateFormat("MM_yyyy").format(_focusedDay)]
+                            as List<dynamic>)
+                        .map((e) => e.toString())
+                        .toList();
+                  }
+
                   return FutureBuilder(
                       future: SpendingFirebase.getSpendingList(list),
                       builder: (context, snapshot) {
@@ -58,74 +59,21 @@ class _CalendarPageState extends State<CalendarPage> {
 
                           return Column(
                             children: [
-                              Card(
-                                child: TableCalendar(
-                                  firstDay: DateTime.utc(2000),
-                                  lastDay: DateTime.utc(2100),
-                                  focusedDay: _focusedDay,
-                                  selectedDayPredicate: (day) =>
-                                      isSameDay(_selectedDay, day),
-                                  calendarFormat: _format,
-                                  onPageChanged: (focusedDay) =>
-                                      _focusedDay = focusedDay,
-                                  onFormatChanged: (format) =>
-                                      setState(() => _format = format),
-                                  startingDayOfWeek: StartingDayOfWeek.monday,
-                                  onDaySelected: (selectedDay, focusedDay) {
-                                    if (!isSameDay(_selectedDay, selectedDay)) {
-                                      setState(() {
-                                        _focusedDay = focusedDay;
-                                        _selectedDay = selectedDay;
-                                      });
-                                    }
-                                  },
-                                  eventLoader: (day) {
-                                    return dataSpending
-                                        .where((element) =>
-                                            isSameDay(element.dateTime, day))
-                                        .toList();
-                                  },
-                                  calendarBuilders: CalendarBuilders(
-                                    markerBuilder: (context, day, events) {
-                                      if (events.isNotEmpty) {
-                                        return Positioned(
-                                          right: 0,
-                                          child: Container(
-                                            width: 20,
-                                            height: 20,
-                                            alignment: Alignment.center,
-                                            decoration: BoxDecoration(
-                                              color: (isSameDay(
-                                                          day, _selectedDay) ||
-                                                      isSameDay(
-                                                          day, DateTime.now()))
-                                                  ? Colors.orange
-                                                  : Colors.blue,
-                                              borderRadius:
-                                                  BorderRadius.circular(90),
-                                            ),
-                                            child:
-                                                Text(events.length.toString()),
-                                          ),
-                                        );
-                                      }
-                                      return null;
-                                    },
-                                    dowBuilder: (context, day) {
-                                      if (day.weekday == DateTime.sunday ||
-                                          day.weekday == DateTime.saturday) {
-                                        return Center(
-                                          child: Text(
-                                            DateFormat.E().format(day),
-                                            style: const TextStyle(
-                                                color: Colors.red),
-                                          ),
-                                        );
-                                      }
-                                      return null;
-                                    },
-                                  ),
-                                ),
+                              customTableCalendar(
+                                focusedDay: _focusedDay,
+                                selectedDay: _selectedDay,
+                                format: _format,
+                                dataSpending: dataSpending,
+                                onPageChanged: (focusedDay) =>
+                                    setState(() => _focusedDay = focusedDay),
+                                onDaySelected: (selectedDay, focusedDay) {
+                                  setState(() {
+                                    _focusedDay = focusedDay;
+                                    _selectedDay = selectedDay;
+                                  });
+                                },
+                                onFormatChanged: (format) =>
+                                    setState(() => _format = format),
                               ),
                               totalSpending(spendingList),
                               Expanded(child: buildSpending(spendingList))
---
+++ lib/page/main/calendar/widget/custom_table_calendar.dart
@@ -0,0 +1,151 @@
+import 'package:expenditure_management/models/spending.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+bool isSameMonth(DateTime day1, DateTime day2) =>
+    day1.year == day2.year && day1.month == day2.month;
+
+Widget customTableCalendar({
+  required DateTime focusedDay,
+  required DateTime selectedDay,
+  required CalendarFormat format,
+  required List<Spending> dataSpending,
+  required Function(DateTime) onPageChanged,
+  required Function(DateTime, DateTime) onDaySelected,
+  required Function(CalendarFormat) onFormatChanged,
+}) {
+  return Card(
+    child: TableCalendar(
+      firstDay: DateTime.utc(2000),
+      lastDay: DateTime.utc(2100),
+      focusedDay: focusedDay,
+      startingDayOfWeek: StartingDayOfWeek.monday,
+      selectedDayPredicate: (day) => isSameDay(selectedDay, day),
+      calendarFormat: format,
+      onPageChanged: (focusedDay) => onPageChanged(focusedDay),
+      onFormatChanged: (format) => onFormatChanged(format),
+      onDaySelected: (mySelectedDay, myFocusedDay) {
+        if (!isSameDay(selectedDay, mySelectedDay) &&
+            isSameMonth(focusedDay, mySelectedDay)) {
+          onDaySelected(mySelectedDay, myFocusedDay);
+        }
+      },
+      eventLoader: (day) {
+        return dataSpending
+            .where((element) => isSameDay(element.dateTime, day))
+            .toList();
+      },
+      calendarBuilders: CalendarBuilders(
+        prioritizedBuilder: (context, day, focusedDay) {
+          if (day.month != focusedDay.month || day.year != focusedDay.year) {
+            return const SizedBox.shrink();
+          }
+          return null;
+        },
+        markerBuilder: (context, day, events) {
+          if (events.isNotEmpty && isSameMonth(focusedDay, day)) {
+            return Positioned(
+              right: 0,
+              child: Container(
+                width: 20,
+                height: 20,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: (isSameDay(day, selectedDay) ||
+                          isSameDay(day, DateTime.now()))
+                      ? Colors.orange
+                      : Colors.blue,
+                  borderRadius: BorderRadius.circular(90),
+                ),
+                child: Text(events.length.toString()),
+              ),
+            );
+          }
+          return const SizedBox.shrink();
+        },
+        dowBuilder: (context, day) {
+          if (day.weekday == DateTime.sunday ||
+              day.weekday == DateTime.saturday) {
+            return Center(
+              child: Text(
+                DateFormat.E().format(day),
+                style: const TextStyle(color: Colors.red),
+              ),
+            );
+          }
+          return null;
+        },
+      ),
+    ),
+  );
+}
+
+/// lưu lại mẫu để có lỗi còn biết sữa :v
+
+/*Card(
+      child: TableCalendar(
+        firstDay: DateTime.utc(2000),
+        lastDay: DateTime.utc(2100),
+        focusedDay: _focusedDay,
+        startingDayOfWeek: StartingDayOfWeek.monday,
+        selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+        calendarFormat: _format,
+        onPageChanged: (focusedDay) => setState(() => _focusedDay = focusedDay),
+        onFormatChanged: (format) => setState(() => _format = format),
+        onDaySelected: (selectedDay, focusedDay) {
+          if (!isSameDay(_selectedDay, selectedDay) &&
+              isSameMonth(_focusedDay, selectedDay)) {
+            setState(() {
+              _focusedDay = focusedDay;
+              _selectedDay = selectedDay;
+            });
+          }
+        },
+        eventLoader: (day) {
+          return dataSpending
+              .where((element) => isSameDay(element.dateTime, day))
+              .toList();
+        },
+        calendarBuilders: CalendarBuilders(
+          prioritizedBuilder: (context, day, focusedDay) {
+            if (day.month != focusedDay.month || day.year != focusedDay.year) {
+              return const SizedBox.shrink();
+            }
+          },
+          markerBuilder: (context, day, events) {
+            if (events.isNotEmpty && isSameMonth(_focusedDay, day)) {
+              return Positioned(
+                right: 0,
+                child: Container(
+                  width: 20,
+                  height: 20,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: (isSameDay(day, _selectedDay) ||
+                            isSameDay(day, DateTime.now()))
+                        ? Colors.orange
+                        : Colors.blue,
+                    borderRadius: BorderRadius.circular(90),
+                  ),
+                  child: Text(events.length.toString()),
+                ),
+              );
+            }
+            return const SizedBox.shrink();
+          },
+          dowBuilder: (context, day) {
+            if (day.weekday == DateTime.sunday ||
+                day.weekday == DateTime.saturday) {
+              return Center(
+                child: Text(
+                  DateFormat.E().format(day),
+                  style: const TextStyle(color: Colors.red),
+                ),
+              );
+            }
+            return null;
+          },
+        ),
+      ),
+    ),*/
