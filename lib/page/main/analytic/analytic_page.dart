a159c383cc6e84f34eb0126a93032558b2b92766
Trần Ngọc Tiến <94039133+ngoctienUIT@users.noreply.github.com>
2022-10-09 10:08:02 +0700

    update UI analytic
    cập nhật UI của thống kê

--- lib/page/main/analytic/analytic_page.dart
+++ lib/page/main/analytic/analytic_page.dart
@@ -1,3 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:expenditure_management/constants/function/get_date.dart';
+import 'package:expenditure_management/page/main/analytic/chart/pie_chart.dart';
+import 'package:expenditure_management/page/main/analytic/show_date.dart';
 import 'package:flutter/material.dart';
 import 'package:expenditure_management/constants/app_styles.dart';
 
@@ -11,10 +15,27 @@ class AnalyticPage extends StatefulWidget {
 class _AnalyticPageState extends State<AnalyticPage>
     with TickerProviderStateMixin {
   late TabController _tabController;
+  bool chart = true;
+  DateTime now = DateTime.now();
+  String date = "";
 
   @override
   void initState() {
+    date = getWeek(now);
     _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(() {
+      now = DateTime.now();
+      setState(() {
+        if (_tabController.index == 0) {
+          date = getWeek(now);
+        } else if (_tabController.index == 1) {
+          date = getMonth(now);
+        } else {
+          date = getYear(now);
+        }
+      });
+    });
+
     super.initState();
   }
 
@@ -60,6 +81,39 @@ class _AnalyticPageState extends State<AnalyticPage>
                       Tab(text: "Yearly")
                     ]),
               ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: StreamBuilder<DocumentSnapshot>(
+                    stream: null,
+                    builder: (context, snapshot) {
+                      return Card(
+                        // elevation: 0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4)),
+                        color: const Color(0xff2c4260),
+                        child: Column(
+                          children: [
+                            const SizedBox(height: 10),
+                            showDate(
+                              date: date,
+                              index: _tabController.index,
+                              now: now,
+                              action: (date) {
+                                setState(() => this.date = date);
+                              },
+                            ),
+                            chart ? const MyPieChart() : const MyPieChart(),
+                          ],
+                        ),
+                      );
+                    }),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() => chart = !chart);
+                },
+                child: const Text("change"),
+              )
             ],
           ),
         ),
