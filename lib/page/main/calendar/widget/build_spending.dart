9eea6acd27a79ebe137928993f66928705ed09d0
Trần Ngọc Tiến <94039133+ngoctienUIT@users.noreply.github.com>
2022-11-02 08:49:02 +0700

    auto update spending calendar
    tự động cập nhật lại khi chỉnh sửa chi tiêu trong trang lịch

--- lib/page/main/calendar/widget/build_spending.dart
+++ lib/page/main/calendar/widget/build_spending.dart
@@ -5,75 +5,86 @@ import 'package:expenditure_management/page/main/home/widget/item_spending_widge
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-Widget buildSpending({List<Spending>? spendingList, DateTime? date}) {
-  return spendingList != null
-      ? (spendingList.isEmpty
-          ? Center(
-              child: Text(
-                "Bạn không có bất kỳ chi tiêu nào\nvào ngày ${DateFormat("dd/MM/yyyy").format(date!)}",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.red.withOpacity(0.7),
-                ),
-                textAlign: TextAlign.center,
-              ),
-            )
-          : showListSpending(spendingList))
-      : loadingItemSpending();
-}
+class BuildSpending extends StatelessWidget {
+  const BuildSpending({Key? key, this.spendingList, this.date, this.change})
+      : super(key: key);
+  final List<Spending>? spendingList;
+  final DateTime? date;
+  final Function(Spending spending)? change;
 
-Widget showListSpending(List<Spending> spendingList) {
-  var numberFormat = NumberFormat.currency(locale: "vi_VI");
-
-  return ListView.builder(
-    shrinkWrap: true,
-    physics: const NeverScrollableScrollPhysics(),
-    itemCount: spendingList.length,
-    itemBuilder: (context, index) {
-      return InkWell(
-        borderRadius: BorderRadius.circular(15),
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) =>
-                  EditSpendingPage(spending: spendingList[index]),
-            ),
-          );
-        },
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
-            child: Row(
-              children: [
-                Image.asset(
-                  listType[spendingList[index].type]["image"]!,
-                  width: 40,
-                ),
-                const SizedBox(width: 10),
-                Text(
-                  listType[spendingList[index].type]["title"]!,
-                  style: const TextStyle(
-                    fontSize: 16,
+  @override
+  Widget build(BuildContext context) {
+    return spendingList != null
+        ? (spendingList!.isEmpty
+            ? Center(
+                child: Text(
+                  "Bạn không có bất kỳ chi tiêu nào\nvào ngày ${DateFormat("dd/MM/yyyy").format(date!)}",
+                  style: TextStyle(
+                    fontSize: 18,
                     fontWeight: FontWeight.bold,
+                    color: Colors.red.withOpacity(0.7),
                   ),
+                  textAlign: TextAlign.center,
                 ),
-                const Spacer(),
-                Text(
-                  numberFormat.format(spendingList[index].money),
-                  style: const TextStyle(fontSize: 16),
+              )
+            : showListSpending(spendingList!))
+        : loadingItemSpending();
+  }
+
+  Widget showListSpending(List<Spending> spendingList) {
+    var numberFormat = NumberFormat.currency(locale: "vi_VI");
+
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: spendingList.length,
+      itemBuilder: (context, index) {
+        return InkWell(
+          borderRadius: BorderRadius.circular(15),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => EditSpendingPage(
+                  spending: spendingList[index],
+                  change: change,
                 ),
-                const SizedBox(width: 10),
-                const Icon(Icons.arrow_forward_ios_outlined)
-              ],
+              ),
+            );
+          },
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+              child: Row(
+                children: [
+                  Image.asset(
+                    listType[spendingList[index].type]["image"]!,
+                    width: 40,
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                    listType[spendingList[index].type]["title"]!,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Spacer(),
+                  Text(
+                    numberFormat.format(spendingList[index].money),
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(width: 10),
+                  const Icon(Icons.arrow_forward_ios_outlined)
+                ],
+              ),
             ),
           ),
-        ),
-      );
-    },
-  );
+        );
+      },
+    );
+  }
 }
